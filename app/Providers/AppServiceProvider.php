cdd2e7b785589636565f0303df906d90e60b5f6a
Erie Dias <eriedias7@gmail.com>
2018-12-29 13:17:53 -0300

    Configurações iniciais e migration/tabela de usuários

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*
+         * Laravel usa o conjunto de caracteres utf8mb4 por padrão, que inclui suporte para armazenar "emojis"
+         *  no banco de dados. Se você estiver executando uma versão do MySQL mais antiga do que a versão 5.7.7
+         *  ou MariaDB anterior à versão 10.2.2, talvez seja necessário configurar manualmente o comprimento da
+         *  string padrão gerado pelas migrações para que o MySQL crie índices para elas. Você pode configurar
+         *  isso chamando o método Schema::defaultStringLength no AppServiceProvider:
+         */
+        Schema::defaultStringLength(191);
     }
 
     /**
